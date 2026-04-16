2a7019c8b84e87540ba0cbc9fd4fe56ae15adead
Nhut <lecongminhnhut6533@gmail.com>
2026-04-16 22:54:58 +0700

    Update for safe

--- part3/benchmark.py
+++ part3/benchmark.py
@@ -2,6 +2,14 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+if parent_dir not in sys.path:
+	sys.path.append(parent_dir)
+
 # Import các module từ project của bạn
 from part1.gauss import gaussian_eliminate
 from part2.decomposition import svd
